eeccf5f52131507d317ad7f708825e645cc58a4a
Harold Jennett <haroldjennett@outlook.com>
2020-09-05 12:38:00 -0400

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+interface AppState {
+  message: string;
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ngrxFire';
+export class AppComponent implements OnInit{
+
+  message$: Observable<string>
+
+  constructor(private store: Store<AppState>){
+
+  }
+
+  spanishMessage(){
+    this.store.dispatch({type: 'SPANISH'})
+  }
+
+  frenchMessage(){
+    this.store.dispatch({type: 'FRENCH'})
+  }
+  englishMessage(){
+    this.store.dispatch({type: 'ENGLISH'})
+  }
+
+  ngOnInit(){
+    this.message$ = this.store.select('message');
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { StoreModule } from '@ngrx/store';
+import { simpleReducer } from './simple.reducer';
+
+
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +12,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    StoreModule.forRoot({ message: simpleReducer })
   ],
   providers: [],
   bootstrap: [AppComponent]
